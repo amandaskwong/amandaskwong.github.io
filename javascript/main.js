d634cf7e4e6fec242a41a40757604938209f9117
amandaskwong <amanda.kwong@edu.sait.ca>
2024-11-14 21:08:11 -0700

    docs: create greeting conditional

--- javascript/main.js
+++ javascript/main.js
@@ -8,3 +8,14 @@ footerYear.textContent = getDate.getFullYear();
 // print date to console
 console.log(getDate.toString())
 
+// greeting
+const greeting = document.getElementById('greeting')
+const hour = getDate.getHours();
+
+if(hour <= 11) {
+    greeting.textContent = 'Good morning!'
+} if(hour >= 12 && hour < 17) {
+    greeting.textContent = 'Good afternoon!'
+} if(hour >= 17) {
+    greeting.textContent = 'Good evening!'
+}
