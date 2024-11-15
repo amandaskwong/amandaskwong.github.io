9b05689e5488a6809dd7d6600eb45435c649ccac
amandaskwong <amanda.kwong@edu.sait.ca>
2024-11-14 20:44:53 -0700

    docs: add date and time to console

--- javascript/main.js
+++ javascript/main.js
@@ -1,6 +1,10 @@
 console.log('hi');
 
+// footer year
 const footerYear = document.getElementById('year');
 const getDate = new Date();
 footerYear.textContent = getDate.getFullYear();
+
+// print date to console
+console.log(getDate.toString())
 
